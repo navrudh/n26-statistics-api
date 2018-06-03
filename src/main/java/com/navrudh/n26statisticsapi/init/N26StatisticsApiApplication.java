9d38d167ec14d4e90dac22f6b3ba192b89474cb9
Dhruvan Ganesh <navrudh@gmail.com>
2018-06-03 11:27:47 +0530

    configure spring boot to follow GMT, enable scheduling

--- src/main/java/com/navrudh/n26statisticsapi/init/N26StatisticsApiApplication.java
+++ src/main/java/com/navrudh/n26statisticsapi/init/N26StatisticsApiApplication.java
@@ -3,10 +3,20 @@ package com.navrudh.n26statisticsapi.init;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @ComponentScan(basePackages = "com.navrudh.n26statisticsapi")
 @SpringBootApplication
+@EnableScheduling
 public class N26StatisticsApiApplication {
+
+    @PostConstruct
+    private void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(N26StatisticsApiApplication.class, args);
